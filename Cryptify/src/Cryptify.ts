79541f564a615f6f818c4f398a581a60f01880a2
Aidy <microsoft@aidanjenkins.co.uk>
2019-02-26 01:24:21 +0000

    Added support for self-decrypting files

--- Cryptify/src/Cryptify.ts
+++ Cryptify/src/Cryptify.ts
@@ -6,6 +6,16 @@
     private static get ALGORITHM_PARAMS() { return { name: "AES-CTR", counter: crypto.getRandomValues(new Uint8Array(16)), length: 128 } as AesCtrParams; }
 
     public static async encryptFile(saveKeyfile = false) {
+        let { data, filename, key } = await this.encryptFile_internal();
+
+        await FileManager.downloadFile(filename + '.ENCRYPTED', data);
+        if (saveKeyfile)
+            await FileManager.downloadFile(filename + '.KEY', uint8ArrayFromString(key));
+        else
+            (document.getElementById("txtKey") as HTMLInputElement).value = key;
+    }
+
+    protected static async encryptFile_internal() {
         try {
             let algorithm = this.ALGORITHM_PARAMS;
             let key = (await crypto.subtle.generateKey({ name: algorithm.name, length: algorithm.length }, true, ["encrypt", "decrypt"])) as CryptoKey;
@@ -13,21 +23,22 @@
 
             let encrypted_data = await crypto.subtle.encrypt(algorithm, key, data);
 
-            let rawKey = btoa(String.fromCharCode(...new Uint8Array(await crypto.subtle.exportKey("raw", key))));
-            rawKey += ';' + btoa(String.fromCharCode(...algorithm.counter as Uint8Array));
+            let sKey = btoa(stringFromArrayBuffer(await crypto.subtle.exportKey("raw", key)));
+            sKey += ';' + btoa(stringFromUint8Array(algorithm.counter as Uint8Array));
 
-            await FileManager.downloadFile(filename + '.ENCRYPTED', encrypted_data);
-            if (saveKeyfile)
-                await FileManager.downloadFile(filename + '.KEY', new Uint8Array(Array(rawKey.length).fill(0).map((_, idx) => rawKey.charCodeAt(idx))));
-            else
-                (document.getElementById("txtKey") as HTMLInputElement).value = rawKey;
+            return { data: encrypted_data, filename: filename + '.ENCRYPTED', key: sKey };
         }
         catch (err) {
             console.log(err);
         }
     }
 
-    public static async decryptFile(filename = null as string) {
+    public static async decryptFile(filename = null as string, content = null as Uint8Array | ArrayBuffer) {
+        let { filename: fName, data } = await this.decryptFile_internal(filename, content);
+        await FileManager.downloadFile(fName, data);
+    }
+
+    protected static async decryptFile_internal(filename = null as string, content = null as Uint8Array | ArrayBuffer) {
         try {
             let algorithm = this.ALGORITHM_PARAMS;
             
@@ -35,40 +46,82 @@
             let counter: Uint8Array;
             let sRawKey = (document.getElementById("txtKey") as HTMLInputElement).value;
             if (sRawKey)
-                rawKey = new Uint8Array(Array(sRawKey.length).fill(0).map((_, idx) => sRawKey.charCodeAt(idx)));
+                rawKey = uint8ArrayFromString(sRawKey);
             else {
                 let { data } = await FileManager.uploadFile(".KEY");
                 rawKey = new Uint8Array(data);
             }
 
-            let [sKey, sCounter] = String.fromCharCode(...new Uint8Array(rawKey)).split(';');
+            let [sKey, sCounter] = stringFromUint8Array(rawKey).split(';');
 
             sKey = atob(sKey);
             sCounter = atob(sCounter);
-            rawKey = new Uint8Array(Array(sKey.length).fill(0).map((_, idx) => sKey.charCodeAt(idx)));
-            counter = new Uint8Array(Array(sCounter.length).fill(0).map((_, idx) => sCounter.charCodeAt(idx)));
+            rawKey = uint8ArrayFromString(sKey);
+            counter = uint8ArrayFromString(sCounter);
 
             algorithm.counter = counter;
 
-            let data: ArrayBuffer;
-            if (filename)
-                data = await (await fetch(filename)).arrayBuffer();
-            else {
-                ({ data, filename } = await FileManager.uploadFile(".ENCRYPTED"));
+            if (!content) {
+                if (filename)
+                    content = await (await fetch(filename)).arrayBuffer();
+                else {
+                    ({ data: content, filename } = await FileManager.uploadFile(".ENCRYPTED"));
+                }
+            }
+            else if (!filename) {
+                filename = "decryptedfile.ENCRYPTED";
             }
 
             let key = await crypto.subtle.importKey("raw", rawKey, algorithm, true, ["encrypt", "decrypt"]);
 
-            let decrypted_data = await crypto.subtle.decrypt(algorithm, key, data);
+            let decrypted_data = await crypto.subtle.decrypt(algorithm, key, content);
 
-            await FileManager.downloadFile(filename.substr(0, filename.lastIndexOf('.')), decrypted_data);
+            return { filename: filename.substr(0, filename.lastIndexOf('.')), data: decrypted_data };
         }
         catch (err) {
             console.log(err);
         }
     }
 
-    public static async generateSelfDecrypt() {
+    public static async generateSelfDecrypt(saveKeyfile = false) {
+        const html = document.createElement("html");
+        const head = document.createElement("head");
+        const body = document.createElement("body");
+        const span = document.createElement("span");
+        const txtKey = document.createElement("input");
+        txtKey.id = "txtKey";
+        txtKey.type = "text";
+        const btnDecrypt = document.createElement("input");
+        btnDecrypt.type = "button";
+        btnDecrypt.value = "Decrypt file";
+        btnDecrypt.id = "btnDecrypt";
+
+        let { data, filename, key } = await this.encryptFile_internal();
+
+        (document.getElementById("txtKey") as HTMLInputElement).value = key;
+
+        span.id = "spData";
+        span.dataset["filename"] = filename;
+        span.textContent = btoa(stringFromArrayBuffer(data));
+        span.style.display = "none";
+
+        html.appendChild(head);
+        html.appendChild(body);
+        body.appendChild(span);
+        body.appendChild(txtKey);
+        body.appendChild(btnDecrypt);
+
+        for (let scr of Array.from(document.getElementsByTagName("script"))) {
+            let script = document.createElement("script");
+            script.type = "text/javascript";
+            script.textContent = await fetch(scr.src).then(x => x.text());
+            head.appendChild(script);
+        }
+        let scrip = document.createElement("script");
+        scrip.type = "text/javascript";
+        scrip.textContent = "setTimeout(() => document.getElementById('btnDecrypt').onclick = e => Cryptify.decryptFile('" + filename + "', uint8ArrayFromString(atob(document.getElementById('spData').textContent))));";
+        head.appendChild(scrip);
+        await FileManager.downloadFile(filename + ".decryptor.html", uint8ArrayFromString(html.outerHTML));
         
     }
 }
